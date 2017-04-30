940b2a627822e2801b2f7de9248793c5a21bb457
Pradeestark <Pradeestarck@gmail.com>
2017-04-30 22:02:59 +0530

    Update and rename f.c to e.c

---
+++ e.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+char c;
+printf("enter the value");
+scanf(%c,&c);
+  if(c>='a'&&c<='z'||c>='A'&&c<='Z')
+  {
+    if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||C=='E'||c=='I'||c=='O'||c=='U')
+      printf("the value is vowel");
+    else
+printf("the value is a consonent");
+}
+  else 
+    printf("invalid entry");
+}
