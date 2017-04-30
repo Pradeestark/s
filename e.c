7306a69ddfb3da01d5f7d16ef948401fb83004c9
Pradeestark <Pradeestarck@gmail.com>
2017-04-30 22:14:50 +0530

    Update e.c

--- e.c
+++ e.c
@@ -3,7 +3,7 @@ void main()
 {
 char c;
 printf("enter the value");
-scanf(%c,&c);
+scanf("%c",&c);
   if(c>='a'&&c<='z'||c>='A'&&c<='Z')
   {
     if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||C=='E'||c=='I'||c=='O'||c=='U')
