d2096de43129e0a90544705ac53bd0e217a559ce
Pradeestark <Pradeestarck@gmail.com>
2017-04-30 22:36:54 +0530

    Update e.c

--- e.c
+++ e.c
@@ -6,7 +6,7 @@ printf("enter the value");
 scanf("%c",&c);
   if(c>='a'&&c<='z'||c>='A'&&c<='Z')
   {
-    if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||C=='E'||c=='I'||c=='O'||c=='U')
+    if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
       printf("the value is vowel");
     else
 printf("the value is a consonent");
